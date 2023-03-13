cce6e519ccf0b2c5a96b9cc26f1eb06068d661f5
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-03-13 11:18:17 +0330

    Import from web

--- src/main/java/org/mm/InterfaceServer/InterfaceServer.java
+++ src/main/java/org/mm/InterfaceServer/InterfaceServer.java
@@ -8,6 +8,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import io.javalin.Javalin;
 import org.apache.commons.io.FileUtils;
+import org.mm.Baloot.Commodity;
+import org.mm.Baloot.Provider;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -30,8 +32,8 @@ public class InterfaceServer {
             importCommoditiesFromWeb(COMMODITIES_URL);
             System.out.println("Importing Providers...");
             importProvidersFromWeb(PROVIDERS_URL);
-            System.out.println("Importing Comments...");
-            importCommentsFromWeb(COMMENTS_URL);
+//            System.out.println("Importing Comments...");
+//            importCommentsFromWeb(COMMENTS_URL);
             runServer(port);
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -41,18 +43,65 @@ public class InterfaceServer {
     private void runServer(int port) {
     }
 
-    private void importUsersFromWeb(String providers_url) {
+    private void importUsersFromWeb(String usersUrl) throws Exception{
+        String UsersJsonString = HTTPRequestHandler.getRequest(usersURL);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<User> users = gson.fromJson(UsersJsonString, new TypeToken<List<User>>() {}.getType());
+        for (User user : users) {
+            try {
+                baloot.addUser(user);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private void importCommoditiesFromWeb(String providers_url) {
+    private void importCommoditiesFromWeb(String commoditiesUrl) throws Exception{
+        String CommoditiesJsonString = HTTPRequestHandler.getRequest(commoditiesURL);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<Commodity> commodities = gson.fromJson(CommoditiesJsonString, new TypeToken<List<Commodity>>() {}.getType());
+        for (Commodity commodity : commodities) {
+            try {
+                baloot.addCommodity(commodity);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private void importProvidersFromWeb(String providers_url) {
+    private void importProvidersFromWeb(String providersUrl) throws Exception{
+        String ProvidersJsonString = HTTPRequestHandler.getRequest(providersURL);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<Provider> providers = gson.fromJson(ProvidersJsonString, new TypeToken<List<Commodity>>() {}.getType());
+        for (Provider provider : providers) {
+            try {
+                baloot.addProvider(provider);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private void importCommentsFromWeb(String comments_url) {
+//    private void importCommentsFromWeb(String commentsURL) throws Exception{
+//        String CommentsJsonString = HTTPRequestHandler.getRequest(commentsURL);
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//        List<Comment> comments = gson.fromJson(CommentsJsonString, new TypeToken<List<Commodity>>() {}.getType());
+//        for (Comment comment : comments) {
+//            try {
+//                baloot.addComment(comment);
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//    }
 
+    private String readTemplateFile(String fileName) throws Exception{
+        File file = new File(Resources.getResource("templates/" + fileName).toURI());
+        return FileUtils.readFileToString(file, StandardCharsets.UTF_8);
     }
 
+    public void stop() {
+        app.stop();
+    }
 
 }
