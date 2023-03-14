365dd8004d3563bf51dc06eb5b7b66de4e1a6a5d
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-03-14 18:56:25 +0330

    Add tests class

---
+++ src/test/java/org.mm/SearchCommodityByPriceTest.java
@@ -0,0 +1,16 @@
+package org.mm;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.mm.Baloot.Baloot;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SearchCommodityByPriceTest {
+}
