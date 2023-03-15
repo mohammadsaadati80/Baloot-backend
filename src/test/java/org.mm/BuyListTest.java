dcfba9b5ef775cb532e1b70995e8be55e253e2cd
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-03-15 22:12:03 +0330

    Add tests for searchCommodityByPrice

--- src/test/java/org.mm/BuyListTest.java
+++ src/test/java/org.mm/BuyListTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BuyListTest { // TODO
+public class BuyListTest {
     private InterfaceServer interfaceServer;
     private Baloot baloot;
 
--- src/test/java/org.mm/SearchCommodityByPriceTest.java
+++ src/test/java/org.mm/SearchCommodityByPriceTest.java
@@ -3,14 +3,67 @@ package org.mm;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mm.Baloot.Baloot;
+import org.mm.Baloot.Commodity;
+import org.mm.Baloot.Exceptions.InvalidCommandError;
+import org.mm.Baloot.Exceptions.InvalidPriceRangeError;
+import org.mm.InterfaceServer.InterfaceServer;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SearchCommodityByPriceTest { // TODO
+public class SearchCommodityByPriceTest {
+    private InterfaceServer interfaceServer;
+    private Baloot baloot;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        final String USERS_URL = "http://5.253.25.110:5000/api/users";
+        final String COMMODITIES_URL = "http://5.253.25.110:5000/api/commodities";
+        final String PROVIDERS_URL = "http://5.253.25.110:5000/api/providers";
+        final String COMMENTS_URL = "http://5.253.25.110:5000/api/comments";
+        final int PORT = 8080;
+        interfaceServer = new InterfaceServer();
+        interfaceServer.start(USERS_URL, COMMODITIES_URL, PROVIDERS_URL, COMMENTS_URL, PORT);
+        baloot = interfaceServer.getBaloot();
+    }
+
+    @AfterEach
+    void tearDown() {
+        interfaceServer.stop();
+        interfaceServer = null;
+        baloot = null;
+    }
+
+    @Test
+    void getCommoditiesByPrice_InvalidCommand() throws Exception {
+        try {
+            baloot.getCommoditiesByPrice(null, null);
+        } catch (Exception e) {
+            Exception expected = new InvalidCommandError();
+            assertEquals(e.getMessage(), expected.getMessage());
+        }
+    }
+
+    @Test
+    void getCommoditiesByPrice_InvalidPriceRangeError() throws Exception {
+        try {
+            baloot.getCommoditiesByPrice(3, 1);
+        } catch (Exception e) {
+            Exception expected = new InvalidPriceRangeError();
+            assertEquals(e.getMessage(), expected.getMessage());
+        }
+    }
+
+    @Test
+    void getCommoditiesByPrice_ReturnSuccessfully() throws Exception {
+        List<Integer> expectedId = Arrays.asList(1, 2, 4, 5, 29);
+        List<Commodity> result = baloot.getCommoditiesByPrice(10000, 20000);
+        assertEquals(5, result.size());
+        for(Commodity commodity : result)
+            assertTrue(expectedId.contains(commodity.getId()));
+    }
+
 }
