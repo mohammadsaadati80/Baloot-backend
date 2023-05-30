6ff55b9149d7ee8268a4c60990165678c398b673
Mohammad Saadati <65104833+mohammadsaadati80@users.noreply.github.com>
2023-05-30 09:18:53 +0330

    Update pom.xml

--- src/main/java/org/mm/BalootApplication.java
+++ src/main/java/org/mm/BalootApplication.java
@@ -3,11 +3,10 @@ package org.mm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@ServletComponentScan
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 public class BalootApplication {
     public static void main(String[] args) {
